afda0143d0e92746e41ee82d521f6b4f5e890e2a
fengjutian <>
2023-09-11 14:14:25 +0800

    save

--- src/elements/Text.ts
+++ src/elements/Text.ts
@@ -1,11 +1,48 @@
 class Text{
+    text: HTMLSpanElement
 
     constructor() {
-
+        this.text = document.createElement('span')
     }
 
-    init() {
-        return `<div>Text</div>`;
+    init(data: string) {
+        this.text.innerText= data
+        return this;
+    }
+
+    style(data: object) {
+
+        
+
+        return this
+    }
+
+    event(events: any) {
+        console.log('events', events)
+
+        // for(let [k, v] of Object.entries(events)) {
+        //     this.text.addEventListener(String(k).toLowerCase(), v)
+        // }
+
+        console.log('this.text', this.text)
+
+        this.text.onclick = _ => () => {
+            console.log(111)
+        }
+
+        // this.text.setAttribute('onclick', `() => {
+        //     console.log(111)
+        // }`)
+
+    
+        return this
+    }
+
+    render() {
+        console.log('this.text',  this.text)
+        return this.text.outerHTML
     }
 
 }
+
+export default Text;
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,30 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 import View from './View.ts'
+import Text from './elements/Text.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  ${new View().init()}
+  ${
+    new Text().init('Hello World').style({
+      style: {
+        color: "red"
+      },
+      class: []
+    }).event({
+      onClick: () => { console.log('click')}
+    }).render()
+  }
+  ${
+    new Text().init('Hello World').style({
+      style: {
+        color: "red"
+      },
+      class: []
+    }).event({
+      onClick: () => { console.log('click')}
+    }).render()
+  }
   </div>
 `
 
